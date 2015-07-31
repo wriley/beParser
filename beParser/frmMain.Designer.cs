6cecc87573e0abc9571cda12602d30eb524fe009
wriley <wriley@riotgames.com>
2015-07-31 22:41:16 +0100

    initial framework

---
+++ beParser/frmMain.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace beParser
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbOutput = new System.Windows.Forms.RichTextBox();
+            this.rtbDebug = new System.Windows.Forms.RichTextBox();
+            this.btnRCON = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtbOutput
+            // 
+            this.rtbOutput.Location = new System.Drawing.Point(12, 41);
+            this.rtbOutput.Name = "rtbOutput";
+            this.rtbOutput.ReadOnly = true;
+            this.rtbOutput.Size = new System.Drawing.Size(912, 510);
+            this.rtbOutput.TabIndex = 0;
+            this.rtbOutput.Text = "";
+            // 
+            // rtbDebug
+            // 
+            this.rtbDebug.Location = new System.Drawing.Point(12, 557);
+            this.rtbDebug.Name = "rtbDebug";
+            this.rtbDebug.ReadOnly = true;
+            this.rtbDebug.Size = new System.Drawing.Size(912, 90);
+            this.rtbDebug.TabIndex = 1;
+            this.rtbDebug.Text = "";
+            // 
+            // btnRCON
+            // 
+            this.btnRCON.Location = new System.Drawing.Point(12, 12);
+            this.btnRCON.Name = "btnRCON";
+            this.btnRCON.Size = new System.Drawing.Size(75, 23);
+            this.btnRCON.TabIndex = 2;
+            this.btnRCON.Text = "RCON";
+            this.btnRCON.UseVisualStyleBackColor = true;
+            // 
+            // fmrMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(936, 659);
+            this.Controls.Add(this.btnRCON);
+            this.Controls.Add(this.rtbDebug);
+            this.Controls.Add(this.rtbOutput);
+            this.Name = "fmrMain";
+            this.Text = "beParser";
+            this.Load += new System.EventHandler(this.fmrMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbOutput;
+        private System.Windows.Forms.RichTextBox rtbDebug;
+        private System.Windows.Forms.Button btnRCON;
+
+    }
+}
+
---
+++ beParser/frmMain.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace beParser
+{
+    public partial class frmMain : Form
+    {
+        List<String> filesToWatch = new List<String>();
+        List<Worker> workerObjects = new List<Worker>();
+        List<Thread> workerThreads = new List<Thread>();
+        String basePath = "testlogs";
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void fmrMain_Load(object sender, EventArgs e)
+        {
+            filesToWatch.Add("scripts.log");
+            filesToWatch.Add("arma2oaserver.RPT");
+            filesToWatch.Add("attachto.log");
+            filesToWatch.Add("publicvariable.log");
+            filesToWatch.Add("setvariable.log");
+
+            logDebug("LOADED");
+
+            Run();
+        }
+
+        private void Run()
+        {
+            foreach(String file in filesToWatch)
+            {
+                Worker w = new Worker(this, basePath + "\\" + file);
+                workerObjects.Add(w);
+                Thread t = new Thread(w.DoWork);
+                workerThreads.Add(t);
+                t.IsBackground = true;
+                t.Start();
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            foreach(Worker w in workerObjects)
+            {
+                w.RequestStop();
+            }
+
+            base.OnFormClosing(e);
+        }
+
+        delegate void logDebugDelegate(String s);
+
+        public void logDebug(String s)
+        {
+            if (InvokeRequired)
+            {
+                logDebugDelegate del = logDebug;
+                Invoke(del, new object[]{s});
+            }
+            else
+            {
+                rtbOutput.Text += addDateString(s);
+            }
+        }
+
+        private String getDateString()
+        {
+            return DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+
+        private String addDateString(String s)
+        {
+            return getDateString() + " " + s + "\n";
+        }
+    }
+
+    public class Worker
+    {
+        private volatile bool _shouldStop;
+        private frmMain _parentForm;
+        private String _filePath;
+        private FileStream _fileStream;
+
+        public Worker(frmMain parentForm, String filePath)
+        {
+            this._parentForm = parentForm;
+            this._filePath = filePath;
+        }
+
+        public void DoWork()
+        {
+            threadLogDebug("starting");
+
+
+            while(!_shouldStop)
+            {
+                threadLogDebug("sleeping");
+                Thread.Sleep(1000);
+            }
+            threadLogDebug("exiting");
+        }
+
+        private void threadLogDebug(String s)
+        {
+            _parentForm.logDebug("Thread " + System.Threading.Thread.CurrentThread.ManagedThreadId.ToString() + " " + s);
+        }
+
+        public void RequestStop()
+        {
+            _shouldStop = true;
+        }
+    }
+}
